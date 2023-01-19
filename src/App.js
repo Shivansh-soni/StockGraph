02008ae809a88f7cc983b7f70182cd1b40e8aa70
Shivansh-soni <shivanshsoni.indore@gmail.com>
2023-01-19 22:27:33 +0530

    INITIAL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import Hero from "./component/Hero";
+
+import React from "react";
+
+import Stock from "./components/Stock"
 
 const App = () => {
+ 
   return (
-    <div className="App flex flex-col lg:flex-row lg:mt-20 justify-center items-center gap-10">
-      <Hero />
+    <div className="App flex flex-col lg:flex-row lg:mt-20 justify-center items-center gap-10">   
+      <Stock></Stock>
     </div>
   );
 };
---
+++ src/components/Stock.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import CanvasJSReact from "../canvasjs.react";
+const FinalArray = [];
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+
+class App extends Component {
+    componentDidMount() {
+        var chart = this.chart;
+
+        
+
+        fetch(
+          "https://financialmodelingprep.com/api/v3/historical-chart/1hour/AAPL?apikey=b1360803f80dd08bdd0211c5c004ad03"
+        )
+          .then(function (response) {
+            return response.json();
+          })
+            .then(function (data) {
+              // eslint-disable-next-line
+              data.map((d) => {
+                //   console.log(new Date(d.date).getTime())
+                const date = new Date(d.date);
+                const yData = [d.open, d.low, d.high, d.close];
+                FinalArray.push({
+                  x: date,
+                  y: yData,
+                });
+              });
+              chart.render();
+            });
+        console.log("FINAL", FinalArray);
+    }
+
+    render() {
+
+        
+
+        const options = {
+          exportEnabled: true,
+          title: {
+            text: "DEMO",
+          },
+          axisX: {
+            valueFormatString: "D MMM",
+          },
+          axisY: {
+            title: "Price",
+            prefix: "$",
+          },
+          data: [
+            {
+              type: "candlestick",
+              name: "DEMO",
+              showInLegend: true,
+              yValueFormatString: "$##0.00",
+              xValueType: "DateTime",
+              dataPoints: FinalArray,
+            },
+          ],
+        };
+        return (
+            <div style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width:"100vw",
+                height:"100vh"
+            }}>
+                <CanvasJSChart options={options} onRef={(ref) => (this.chart = ref)}  />
+            </div>
+        );
+    }
+}
+export default App;
